5b484d5e76bf62ce448b19b9b7de49d10f41a05f
kk2216 <kk2216@ic.ac.uk>
2020-05-07 01:11:39 +0100

    Reformat to object

--- simulation.py
+++ simulation.py
@@ -7,6 +7,48 @@ class Agent:
         self.id = id
         self.adopted = False
 
+class Simulation:
+    def __init__(self, initGraph):
+        self.G = initGraph
+        self.tick = 0
+
+    def simulate(self):
+        pass
+
+    def drawGraphState(self):
+        # Get pos of nodes
+        pos=nx.spectral_layout(self.G)
+
+        # Find nodes which have adopted product
+        adopted = [node for node in self.G.nodes() if node.adopted]
+        notAdopted = [node for node in self.G.nodes() if node not in adopted]
+        
+        # Draw Adopted nodes
+        nx.draw_networkx_nodes(self.G, pos,
+                        nodelist=adopted,
+                        node_color='g',
+                        node_size=500,
+                        alpha=0.8)
+        # Draw non adotped nodes                   
+        nx.draw_networkx_nodes(self.G, pos,
+                        nodelist=notAdopted,
+                        node_color='r',
+                        node_size=500,
+                        alpha=0.8)
+
+        # Draw edges
+        nx.draw_networkx_edges(self.G, pos, width=1.0, alpha=0.5)
+
+        # Labels for nodes
+        labels = {node:node.id for node in self.G.nodes()}               
+
+        # Draw labels
+        nx.draw_networkx_labels(self.G, pos, labels, font_size=16)
+
+        plt.savefig("./fig_tick{}.png".format(self.tick))
+
+
+
 if __name__ == "__main__":
 
     #Set up graph
@@ -21,34 +63,5 @@ if __name__ == "__main__":
         (nodes[0], nodes[3]),
     ])
 
-    
-    # Get pos of nodes
-    pos=nx.spring_layout(G)
-
-    # Find nodes which have adopted product
-    adopted = [node for node in G.nodes() if node.adopted]
-    notAdopted = [node for node in G.nodes() if node not in adopted]
-    
-    # Draw Adopted nodes
-    nx.draw_networkx_nodes(G,pos,
-                       nodelist=adopted,
-                       node_color='g',
-                       node_size=500,
-                       alpha=0.8)
-    # Draw non adotped nodes                   
-    nx.draw_networkx_nodes(G,pos,
-                       nodelist=notAdopted,
-                       node_color='r',
-                       node_size=500,
-                       alpha=0.8)
-
-    # Draw edges
-    nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-
-    # Labels for nodes
-    labels = {node:node.id for node in G.nodes()}               
-
-    # Draw labels
-    nx.draw_networkx_labels(G, pos, labels, font_size=16)
-
-    plt.savefig("./fig.png")  
+    sim = Simulation(G)
+    sim.drawGraphState()  
